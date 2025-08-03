9d4c017e64434db94ed6c62aa2fc3b03c8e42b01
Ifti-Alamin <iftialmin10@gmail.com>
2025-08-03 23:32:24 +0600

    ifti/#24/remaining activeDonors and callRecords are successfully splitted

---
+++ badhan-backend-test/tests/activeDonors/schemas.js
@@ -0,0 +1,104 @@
+const postActiveDonorSchema = {
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    status: { type: "string" },
+    statusCode: { const: 201 },
+    message: { type: "string" },
+    newActiveDonor: {
+      type: "object",
+      additionalProperties: false,
+      properties: {
+        _id: { type: "string" },
+        donorId: { type: "string" },
+        markerId: { type: "string" },
+        time: { type: "integer" },
+      },
+    },
+  },
+};
+
+const deleteActiveDonorSchema = {
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    status: { type: "string" },
+    statusCode: { const: 200 },
+    message: { type: "string" },
+    removedActiveDonor: {
+      type: "object",
+      additionalProperties: false,
+      properties: {
+        _id: { type: "string" },
+        donorId: { type: "string" },
+        markerId: { type: "string" },
+        time: { type: "integer" },
+      },
+    },
+  },
+};
+
+const activeDonorSearchResultSchema = {
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    status: { type: "string" },
+    statusCode: { const: 200 },
+    message: { type: "string" },
+    activeDonors: {
+      type: "array",
+      minItems: 1,
+      items: {
+        type: "object",
+        additionalProperties: false,
+        properties: {
+          _id: { type: "string" },
+          hall: { type: "integer" },
+          name: { type: "string" },
+          address: { type: "string" },
+          comment: { type: "string" },
+          commentTime: { type: "integer" },
+          lastDonation: { type: "integer" },
+          availableToAll: { type: "boolean" },
+          bloodGroup: { type: "integer" },
+          studentId: { type: "string" },
+          phone: { type: "integer" },
+          markedTime: { type: "integer" },
+          markerName: { type: "string" },
+          donationCount: { type: "integer" },
+          callRecordCount: { type: "integer" },
+          lastCallRecord: {
+            type: {
+              anyOf: [{ type: "integer" }, { type: "null" }],
+            },
+          },
+        },
+        required: [
+          "_id",
+          "hall",
+          "name",
+          "address",
+          "comment",
+          "commentTime",
+          "lastDonation",
+          "availableToAll",
+          "bloodGroup",
+          "studentId",
+          "phone",
+          "markedTime",
+          "markerName",
+          "donationCount",
+          "callRecordCount",
+          "lastCallRecord",
+        ],
+      },
+    },
+  },
+  required: ["status", "statusCode", "message", "activeDonors"],
+};
+
+module.exports = {
+  postActiveDonorSchema,
+  deleteActiveDonorSchema,
+  activeDonorSearchResultSchema,
+};
---
+++ badhan-backend-test/tests/callRecords/success.test.js
@@ -0,0 +1,69 @@
+const { badhanAxios } = require("../../api");
+const validate = require("jsonschema").validate;
+const env = require("../../config");
+const { processError } = require("../fixtures/helpers");
+const { postCallRecordsSchema, deleteCallRecordsSchema } = require("./schemas");
+
+test("POST&DELETE/callrecords: success", async () => {
+  try {
+    //post/callrecords part
+
+    let signInResponse = await badhanAxios.post("/users/signin", {
+      phone: env.SUPERADMIN_PHONE,
+      password: env.SUPERADMIN_PASSWORD,
+    });
+
+    let donorResponse = await badhanAxios.get("/users/me", {
+      headers: {
+        "x-auth": signInResponse.data.token,
+      },
+    });
+
+    let recordCreationResponse = await badhanAxios.post(
+      "/callrecords",
+      {
+        donorId: donorResponse.data.donor._id,
+      },
+      {
+        headers: {
+          "x-auth": signInResponse.data.token,
+        },
+      }
+    );
+
+    let validationRecordResult = validate(
+      recordCreationResponse.data,
+      postCallRecordsSchema
+    );
+
+    expect(validationRecordResult.errors).toEqual([]);
+
+    // delete/donations part
+
+    let donationDeletionResponse = await badhanAxios.delete(
+      "/callrecords?donorId=" +
+        donorResponse.data.donor._id +
+        "&callRecordId=" +
+        recordCreationResponse.data.callRecord["_id"],
+      {
+        headers: {
+          "x-auth": signInResponse.data.token,
+        },
+      }
+    );
+
+    let validationResult = validate(
+      donationDeletionResponse.data,
+      deleteCallRecordsSchema
+    );
+    expect(validationResult.errors).toEqual([]);
+
+    await badhanAxios.delete("/users/signout", {
+      headers: {
+        "x-auth": signInResponse.data.token,
+      },
+    });
+  } catch (e) {
+    throw processError(e);
+  }
+});
